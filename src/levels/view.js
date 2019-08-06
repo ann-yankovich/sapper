1d4d9d75fd61cf11cbbc55d764930994990fe80a
Ann Yankovich <ann.yankovich@gmail.com>
2019-08-06 22:40:04 +0300

    fix error

--- src/levels/view.js
+++ src/levels/view.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
 
 LevelsView.propTypes = {
   levels: PropTypes.array.isRequired
